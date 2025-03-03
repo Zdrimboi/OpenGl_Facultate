5db14a69f69e13a214d2b7af347da1cac49e0251
Zdrimboi <162045987+Zdrimboi@users.noreply.github.com>
2025-03-03 03:12:56 +0200

    Linking, project somewhat finished

--- OpenGl_Facultate/src/main.cpp
+++ OpenGl_Facultate/src/main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <GLFW/glfw3.h>
+
 
 int main () {
 	std::cout << "Hello, World!" << std::endl;
+
+	glfwInit();
+
+	// Set OpenGL version to 3.3
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+	// Set OpenGL profile to core
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// For compatibility with macOS
+# ifdef __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+# endif
+
+
 	return 0;
 }
