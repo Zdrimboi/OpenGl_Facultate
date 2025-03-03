4daaf7035b386317405194e61a637c6ad58a8267
Zdrimboi <162045987+Zdrimboi@users.noreply.github.com>
2025-03-03 03:28:36 +0200

    First OpenGL Window

--- OpenGl_Facultate/src/main.cpp
+++ OpenGl_Facultate/src/main.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+// Function used to handle errors
+void error_callback(int error, const char* description, char data = NULL) {
+	if (data != NULL) {
+		std::cerr << "Error: " << description << " " << data << std::endl;
+		return;
+	}
+	else {
+		std::cerr << "Error: " << description << std::endl;
+	}
+}
 
 int main () {
-	std::cout << "Hello, World!" << std::endl;
-
+	// Initialize GLFW
 	glfwInit();
 
 	// Set OpenGL version to 3.3
@@ -18,6 +28,38 @@ int main () {
 # ifdef __APPLE__
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 # endif
+
+	//Create window
+	GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL", NULL, NULL);
+
+	// Checking if window was created
+	if (window == NULL) {
+		error_callback(-1, "Failed to create window");
+		glfwTerminate();
+		return -1;
+	}
+
+	// Make the window's context current
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) 
+	{
+		error_callback(-1, "Failed to initialize GLAD");
+		return -1;
+	}
+
+	// Set the viewport
+	glViewport(0, 0, 800, 600);
+
+	// Loop until the user closes the window
+	while (!glfwWindowShouldClose(window)) {
+		// Render here
+		glClear(GL_COLOR_BUFFER_BIT);
+		// Swap front and back buffers
+		glfwSwapBuffers(window);
+		// Poll for and process events
+		glfwPollEvents();
+	}
 
 
 	return 0;
